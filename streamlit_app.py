9be4be84a3abba467855fa4d7a10a07d1d0e64c4
TAa64 <121226335+TAa64@users.noreply.github.com>
2023-04-11 17:36:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-my_data_rows = my_data_rows.set_index('fruit_name')
+my_data_rows = my_data_rows.set_index('FRUIT_NAME')
 add_my_fruit = streamlit.multiselect("Add your fruits:", list(my_data_rows.index))
